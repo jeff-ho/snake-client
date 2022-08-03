ea9266388d6f20e2545c64dac9c45ecd14fffb6e
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-03 20:12:28 +0000

    add exit feature

--- client.js
+++ client.js
@@ -3,17 +3,17 @@ const net = require("net");
 const connect = function () {
   const conn = net.createConnection({
     host: "165.227.47.243",
-    port: 50541
+    port: 50541,
   });
 
-  conn.on('connect',() => {
-    console.log('Successfully connected to game server!');
-    conn.write("Name: ___");
-  })
-  
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server!");
+    conn.write("Name: YOO");
+  });
+
   conn.setEncoding("utf8");
 
-  conn.on('data', (data) => {
+  conn.on("data", (data) => {
     console.log(`${data}`);
   });
 
--- play.js
+++ play.js
@@ -1,6 +1,18 @@
-const connect = require('./client');
+const connect = require("./client");
 
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.on("data", handleUserInput);
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  return stdin;
+};
 
-
-
+const handleUserInput = function(data) {
+  if (data === '\u0003') {
+    process.exit();
+  }
+  
+};
 
