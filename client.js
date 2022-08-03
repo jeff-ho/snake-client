b5ac62be0db95ef6eefb987a556f5380e634b555
Jeffrey Ho <jeff.ho@hotmail.com>
2022-08-03 17:59:08 +0000

    sending name to server upon connection

---
+++ client.js
@@ -0,0 +1,26 @@
+const net = require("net");
+
+const connect = function () {
+  const conn = net.createConnection({
+    host: "165.227.47.243",
+    port: 50541
+  });
+
+  conn.on('connect',() => {
+    console.log('Successfully connected to game server!');
+    conn.write("Name: ___");
+  })
+  
+  conn.setEncoding("utf8");
+
+  conn.on('data', (data) => {
+    console.log(`${data}`);
+  });
+
+  return conn;
+};
+
+module.exports = connect;
+
+console.log("Connecting ...");
+connect();
